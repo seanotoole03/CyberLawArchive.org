3b8d73c6690d56220efd6574e09d170a5ffe0723
SeanOTooleBSU <seanotoole@u.boisestate.edu>
2020-03-29 20:46:16 -0600

    Webpage contents absent, testing move to index.html pt. 3

--- index.php
+++ index.php
@@ -1,11 +1,12 @@
 <!-- https://young-bayou-40048.herokuapp.com/ -->
 <?php 
-	//include_once("index.html");
+
 	if(!isset($_SESSION)) {
 		session_start();
 	}
-	require_once 'Dao.php';
+	require_once('Dao.php');
 	$dao = new Dao();
 	
-	require_once 'index.html';
+	include_once("index.html");
+
 ?>
