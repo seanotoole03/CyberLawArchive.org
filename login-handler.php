e0dfea52abc38477d30b489d7993ad0c1862410f
SeanOTooleBSU <seanotoole@u.boisestate.edu>
2020-03-29 21:49:27 -0600

    forgot to add handler for login

---
+++ login-handler.php
@@ -0,0 +1,19 @@
+<?php
+  session_start();
+  require_once("Dao.php");
+  $dao = new Dao();
+  
+  $username = $_POST["username"];
+  $password = $_POST["password"];
+  
+//  if ($username == $_POST['username'] && $password == $_POST['password']) {
+  if( $dao->getLogin($username, $password) != FALSE)
+    $_SESSION['auth'] = true;
+    header("Location: https://young-bayou-40048.herokuapp.com/index.php");
+    exit;
+  } else {
+    $_SESSION['auth'] = false;
+    $_SESSION['message'] = "Invalid username or password";
+    header("Location: https://young-bayou-40048.herokuapp.com/index.php");
+  }
+?>
