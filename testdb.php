290391edfda3bd4d8adb1d25758faa80beb02901
SeanOTooleBSU <seanotoole@u.boisestate.edu>
2020-03-31 17:29:28 -0600

    single quotes break dsn string assembly

--- testdb.php
+++ testdb.php
@@ -8,7 +8,7 @@
   $password = '6bd6d8623ad04c85a8407c24244314680ae89a962e15e546674b78d14c90a9ff';
   $port = '5432';
  
-$dsn = 'pgsql:host=$host;port=5432;dbname=$db;user=$username;password=$password';
+$dsn = "pgsql:host=$host;port=5432;dbname=$db;user=$username;password=$password";
 //$dsn = "pgsql:host=$host;port=5432;dbname=$db"; 
  
 try{
