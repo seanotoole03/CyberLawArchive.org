aba29e617120e1a49726662e8789c3af2a292c36
SeanOTooleBSU <seanotoole@u.boisestate.edu>
2020-03-31 16:12:17 -0600

    unusual response from testdb, replacing user variable to check cause and consistency of results 2

--- testdb.php
+++ testdb.php
@@ -4,7 +4,7 @@
 //require_once 'dbconfig.php';
   $host = 'ec2-184-72-236-57.compute-1.amazonaws.com';
   $dbname = 'd5jnebdbvh02jr';
-  $username = 'postgres';
+  $username = 'rnxnybdobgoqry';
   $password = '6bd6d8623ad04c85a8407c24244314680ae89a962e15e546674b78d14c90a9ff';
   $port = '5432';
  
