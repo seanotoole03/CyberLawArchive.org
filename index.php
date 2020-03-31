9dc6a74f40ef8c0c83fcec18e8f25588988bf078
SeanOTooleBSU <seanotoole@u.boisestate.edu>
2020-03-31 11:55:58 -0600

    test block display

--- index.php
+++ index.php
@@ -26,7 +26,7 @@
 			}
 			
 			function openLogin() {
-			  document.getElementById('login').style.display = "block";
+			  document.getElementById('login').style.display = "none";
 			}
 
 			function closeLogin() {
