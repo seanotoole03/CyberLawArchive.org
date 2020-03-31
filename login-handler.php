bc91fcf4970bc3ae0348f45b6660123f2ed3f088
SeanOTooleBSU <seanotoole@u.boisestate.edu>
2020-03-30 21:28:47 -0600

    tweaking probably incorrect PDO in Dao

--- login-handler.php
+++ login-handler.php
@@ -3,8 +3,10 @@
   require_once("Dao.php");
   $dao = new Dao();
   
-  $username = $_POST["username"];
-  $password = $_POST["password"];
+  //$username = $_POST["username"];
+  //$password = $_POST["password"];
+  $username = 'seano';
+  $password = 'Postgres.Admiral1'
   $user;
   
 //  if ($username == $_POST['username'] && $password == $_POST['password']) {
