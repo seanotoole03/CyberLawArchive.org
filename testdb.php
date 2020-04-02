c7be6ded8dd06b1b76889e47652dcb22047385a8
SeanOTooleBSU <seanotoole@u.boisestate.edu>
2020-04-02 17:02:51 -0600

    another new dns test

--- testdb.php
+++ testdb.php
@@ -1,6 +1,6 @@
 
 <?php
- 
+  $databaseURL = 'jdbc:postgresql://ec2-184-72-236-57.compute-1.amazonaws.com:5432/d5jnebdbvh02jr'
 //require_once 'dbconfig.php';
   $host = 'ec2-184-72-236-57.compute-1.amazonaws.com';
   $dbname = 'd5jnebdbvh02jr';
@@ -8,13 +8,24 @@
   $password = '6bd6d8623ad04c85a8407c24244314680ae89a962e15e546674b78d14c90a9ff';
   $port = '5432';
  
-$dsn = "pgsql:host=$host;port=5432;dbname=$db;user=$username;password=$password";
+//$dsn = "pgsql:host=$host;port=5432;dbname=$db;user=$username;password=$password";
 //$dsn = "pgsql:host=$host;port=5432;dbname=$db"; 
+
  
 try{
  // create a PostgreSQL database connection
- echo $dsn . "\n"; 
- $conn = new PDO($dsn);
+ //echo $dsn . "\n"; 
+ $db = parse_url(getenv($databaseURL));
+
+ $conn = new PDO("pgsql:" . sprintf(
+	"host=%s;port=%s;user=%s;password=%s;dbname=%s",
+	$db["host"],
+	$db["port"],
+	$db["user"],
+	$db["pass"],
+	ltrim($db["path"], "/")
+  ));
+ //$conn = new PDO($dsn);
  
 //  $conn = new PDO($dsn, $username, $password);
  
