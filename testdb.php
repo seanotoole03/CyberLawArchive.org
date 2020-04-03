0ddc9bffc29b3ccef799ff3f99a301894f8f6e29
SeanOTooleBSU <seanotoole@u.boisestate.edu>
2020-04-02 18:20:46 -0600

    url tweak echo 6

--- testdb.php
+++ testdb.php
@@ -17,7 +17,7 @@ try{
  // create a PostgreSQL database connection
  //echo $dsn . "\n"; 
  $db = parse_url(getenv($databaseURL));
- echo $db . "\n"; 
+ //echo $db . "\n"; 
  $conn = new PDO("pgsql:" . sprintf(
 	"host=%s;port=%s;user=%s;password=%s;dbname=%s",
 	$db["host"],
