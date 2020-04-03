36d2b58c5fb0f12daf97a15153d2ffc212120842
SeanOTooleBSU <seanotoole@u.boisestate.edu>
2020-04-02 20:03:10 -0600

    testdb overhaul 2

--- testdb.php
+++ testdb.php
@@ -12,24 +12,24 @@
   $password = '6bd6d8623ad04c85a8407c24244314680ae89a962e15e546674b78d14c90a9ff';
   $port = '5432';
  
-//$dsn = "pgsql:host=$host;port=5432;dbname=$db;user=$username;password=$password";
+$dsn = "pgsql:host=$host;port=5432;dbname=$db;user=$username;password=$password";
 //$dsn = "pgsql:host=$host;port=5432;dbname=$db"; 
 
  
 try{
  // create a PostgreSQL database connection
  //echo $dsn . "\n"; 
- $db = parse_url(getenv($databaseURL));
+ //$db = parse_url(getenv($databaseURL));
  //echo $db . "\n"; 
- $conn = new PDO("pgsql:" . sprintf(
+/* $conn = new PDO("pgsql:" . sprintf(
 	"host=%s;port=%s;user=%s;password=%s;dbname=%s",
 	$db["host"],
 	$db["port"],
 	$db["user"],
 	$db["pass"],
 	ltrim($db["path"], "/")
-  ));
- //$conn = new PDO($dsn);
+  )); */
+ $conn = new PDO($dsn);
  
 //  $conn = new PDO($dsn, $username, $password);
  
