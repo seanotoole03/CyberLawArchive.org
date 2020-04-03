1e13689ab6f44ae3d170bfafbf00cef0c505e2ad
SeanOTooleBSU <seanotoole@u.boisestate.edu>
2020-04-02 20:04:22 -0600

    testdb overhaul 3

--- testdb.php
+++ testdb.php
@@ -12,8 +12,8 @@
   $password = '6bd6d8623ad04c85a8407c24244314680ae89a962e15e546674b78d14c90a9ff';
   $port = '5432';
  
-$dsn = "pgsql:host=$host;port=5432;dbname=$db;user=$username;password=$password";
-//$dsn = "pgsql:host=$host;port=5432;dbname=$db"; 
+//$dsn = "pgsql:host=$host;port=5432;dbname=$db;user=$username;password=$password";
+$dsn = "pgsql:host=$host;port=5432;dbname=$db"; 
 
  
 try{
@@ -29,9 +29,9 @@ try{
 	$db["pass"],
 	ltrim($db["path"], "/")
   )); */
- $conn = new PDO($dsn);
+//$conn = new PDO($dsn);
  
-//  $conn = new PDO($dsn, $username, $password);
+  $conn = new PDO($dsn, $username, $password);
  
  // display a message if connected to the PostgreSQL successfully
  if($conn){
