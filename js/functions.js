fec9b9179053bdbafa7e92aa5c2333e0e4e0e2f8
SeanOTooleBSU <seanotoole@u.boisestate.edu>
2020-04-04 18:42:23 -0600

    add new js folder

---
+++ js/functions.js
@@ -0,0 +1,15 @@
+function openLogin() {
+  document.getElementById('login').style.display = "block";
+}
+
+function closeLogin() {
+  document.getElementById('login').style.display = "none";
+}
+
+function openSignUp() {
+  document.getElementById('signUp').style.display = "block";
+}
+
+function closeSignUp() {
+  document.getElementById('signUp').style.display = "none";
+}
