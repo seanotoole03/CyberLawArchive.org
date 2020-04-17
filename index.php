56e653ea9951e81fae495a9bdd71d9f31ef68a58
SeanOTooleBSU <seanotoole@u.boisestate.edu>
2020-04-16 21:41:01 -0600

    css update

--- index.php
+++ index.php
@@ -44,13 +44,8 @@
 	</head>
 <!--	<header> CyberLawArchive.org </header> -->
 	<body>
-		<div class="top-window">
-			<div class="top-background index"></div>
-				<?php	  /* TEST CODE, REMOVE LATER */
-					  if(isset($_SESSION['userArray'])){
-						 echo "<div class='error'>{$userArray}</div>";
-					  }
-					  /* TEST CODE END*/ ?>
+		<div class="top-window top-background index">
+			<!-- <div class="top-background index"></div> -->
 			<a class="img" href="index.php"> <img class="logo" src="resources/cyber-law.png" alt="a gavel in front of a laptop screen"
 			title="clip art cyber law image from online public domain source"/> </a>
 			<a class="logo" href="index.php"> <logo-text> Cyber Law Archive </logo-text> </a>
