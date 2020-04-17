571cd2341208b03e5bd449668ecc21663bd1c2be
SeanOTooleBSU <seanotoole@u.boisestate.edu>
2020-04-16 18:28:14 -0600

    style changes, hash issues

--- index.php
+++ index.php
@@ -44,8 +44,8 @@
 	</head>
 <!--	<header> CyberLawArchive.org </header> -->
 	<body>
-		<div class="top-window top-background index">
-			<!--<div class="top-background index"></div>-->
+		<div class="top-window">
+			<div class="top-background index"></div>
 			<a class="img" href="index.php"> <img class="logo" src="resources/cyber-law.png" alt="a gavel in front of a laptop screen"
 			title="clip art cyber law image from online public domain source"/> </a>
 			<a class="logo" href="index.php"> <logo-text> Cyber Law Archive </logo-text> </a>
